d0db4432e8b3ebda29ea4b1249fd4d942652afa9
sayeem <sayeems@gmail.com>
2022-11-12 00:00:37 +0600

    products page added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,36 @@
 import React from "react";
+import "../assets/css/main.css";
 import { graphql, Link } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
 
 const Index = (props) => {
-  console.log(props);
+  const {
+    data: {
+      allWpProduct: { nodes: products },
+    },
+  } = props;
+  console.log(products[0]);
   return (
     <div className="container">
       <h1 style={{ textAlign: "center" }}>Gatsby Build Performance Test</h1>
+      <div className="productsContainer">
+        {products.map((product) => (
+          <div className="singleProduct" key={product.slug}>
+            <div className="imageContainer">
+              <GatsbyImage
+                image={product.image.localFile.childImageSharp.gatsbyImageData}
+                alt={product.name}
+              />
+            </div>
+            <div className="productDetails">
+              <h4 className="title">{product.name}</h4>
+              <p className="price">
+                Price: <span className="value">$12</span>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
